02c5b6c7dc346fe70d1827888dcc151ecb9e436d
groggy67 <groggy67@yahoo.com>
2024-10-11 17:31:58 +0100

    Update review_prompting.py
    Simplified the pipeline and adjusted prompt

--- review_prompting.py
+++ review_prompting.py
@@ -1,19 +1,17 @@
 # Testing my local ollama-haystack and local llama
-
+import logging
 import json
+
+from schema import review_schema
+
 from haystack import  Document, Pipeline
 from haystack.components.builders.prompt_builder import PromptBuilder
-from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
-from haystack.document_stores.in_memory import InMemoryDocumentStore
 from haystack_integrations.components.generators.ollama import OllamaGenerator
 
-document_store = InMemoryDocumentStore()
+logging.basicConfig(filename='review.pipeline.log', level=logging.INFO)
+logging.info('Start')
 
-
-text = "This hotel which is literally a few short steps away from Times Square offers a unique high end hotel experience in New York. Exceptionally friendly and helpful staff, very stylish Art Deco interior, beautiful rooms (we had a superior guestroom which was a bit on the light side in terms of space and a very generous junior suite), spectacular bath rooms and a nice spa with a small jet stream pool. Undoubtedly expensive, but highly recommendable. This is a 10 out of 10."
-document_store.write_documents( [ 
-    Document(content=text),
-])
+text = "After so many positive reviews of this hotel there's not much left to say. With that said, here's my 2 cents worth. The Homewood Suites is very comfortable within walking distance to attractions and restaurants. There's a friendly, helpful front desk staff. The breakfast buffet is plentiful and is kept well stocked. If your stay is on a weekday the hotel offers a dinner buffet and complimentary wine and beer. Clean, comfortable rooms and very affordable rates for D.C. I'd stay here again in a minute."
 
 query = """
 Determine if the text contains a sentiment about the following,
@@ -41,13 +39,13 @@ Format your response as a JSON object with the following keys,
 'noise',
 'noise_sentiment',
 'anger_sentiment'
-
+Any sentiment should be either 'positive', 'neutral' or 'negative'.
 The location key value will be the comment about the location of the hotel and nothing else.
 The location_sentiment key value will be the sentiment of the comment about the location of the hotel and nothing else.
 The room key value will be the comment about the room and nothing else.
 The room_sentiment key value will be the sentiment of the comment about the room and nothing else.
-The cleaning key value will be the comment about any dirt, filth, unitidiness, uncleaness, lack or hygiene in the hotel and nothing else.
-The cleaning_sentiment key value will be the sentiment about any dirt, filth, unitidiness, lack or hygiene uncleaness in the hotel and nothing else.
+The cleaning key value will be the comment about any dirt, dirty, soiled, filth, unitidiness, unclean, lack or hygiene, stains in the hotel and nothing else.
+The cleaning_sentiment key value will be the sentiment about any dirt, dirty, soiled, filth, unitidiness, unclean, lack or hygiene, stains  in the hotel and nothing else.
 The food key value will be the comment about the food and nothing else.
 The food_sentiment key value will be the sentiment of the comment about the food and nothing else.
 The staff_service key value will be the comment about the any staff members and nothing else.
@@ -57,39 +55,29 @@ The cost_sentiment key value will be the sentiment of the price of room and or c
 The facilities key value will be the comment about the hotel facilities such as shuttle buses, parking area, gym, swimming pool, public area.
 The facilities_sentiment key value will be the sentiment of the hotel facilities such as shuttle buses, parking area, gym, swimming pool, public area.
 The bed key value will be the sentence which must contain the word the words  'bed' or 'beds' or 'duvet' or 'sheet' or 'sheets' or 'mattress' or 'blankets' or 'pillows'  and nothing else otherwise the bed key value is 'unknown'.
-The bed_sentiment key value will be the sentiment of sentence which must contain the word  'bed' or 'beds' or 'duvet' or 'sheet' or 'sheets' or 'mattress' or 'blankets' or 'pillows'  and nothing else. otherwise the bed_sentiment  key value is 'neutral'.
+The bed_sentiment key value will be the sentiment of sentence which must contain the word  'bed' or 'beds' or 'pillow menu' or 'duvet' or 'sheet' or 'sheets' or 'mattress' or 'blankets' or 'pillows'  and nothing else. otherwise the bed_sentiment  key value is 'neutral'.
 The wifi key value will be the comment about wifi or internet used in hotel and nothing else.
 The wifi_sentiment key value will be the sentiment about wifi or internet used in hotel and nothing else.
 The noise key value will be the sentence containing the words 'noise' or 'noisy'  and nothing else.
-The noise_sentiment key value will be 'negative' if the sentence contains the words 'noise' or 'noisy'  otherwise it will be 'unkown'.
+The noise_sentiment key value will be 'negative' if the sentence contains the words 'noise' or 'noisy'  otherwise it will be 'neutral'.
 The anger_sentiment key value will be the true if the tone  or sentiment of the comment is angry and false if not.
 If the information isn't present, use 'unknown' as the value. 
+If the sentiment is isn't present use 'neutral' as the value.
 Just return the JSON object as the answer. 
 """
 
 template = """  
-Give the following information, answer the question for each document.
+Give the following information, answer the question from the information present in this text: {{text}}.
 Ignore your on knwoledge.
-Context:
-    {% for document in documents %}
-         {{ document.content }}
-    {% endfor %}
-
 Question={{query}}?
 """
 
+
 pipe = Pipeline()
-
-
-pipe.add_component("retriever", InMemoryBM25Retriever(document_store=document_store))
-pipe.add_component("prompt_builder", PromptBuilder(template=template))
+pipe.add_component("prompt_builder", PromptBuilder(template=template ))
 pipe.add_component("llm", OllamaGenerator(model="llama3.2:latest", url="http://localhost:11434"))
-pipe.connect("retriever", "prompt_builder.documents")
 pipe.connect("prompt_builder", "llm")
 
-response = pipe.run({"prompt_builder": {"query": query}, "retriever": {"query": query}})
 
-
+response = result = pipe.run({"prompt_builder": {"query": query, "text": text }})
 print(json.dumps(response["llm"]["replies"], indent=4))
-
-
